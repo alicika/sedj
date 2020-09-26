b901c611d7ce35aee67421ad50de0bdaff9ba35c
alicika <52126032+alicika@users.noreply.github.com>
2020-09-26 23:30:18 +0900

    modified c01/ex08

--- rush021/trimspace_input.c
+++ rush021/trimspace_input.c
@@ -1,35 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*   trimspace_input.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rkasubuc <rkasubuc@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/26 00:41:11 by rkasubuc          #+#    #+#             */
-/*   Updated: 2020/09/26 20:22:48 by rkasubuc         ###   ########.fr       */
+/*   Created: 2020/09/26 15:52:12 by rkasubuc          #+#    #+#             */
+/*   Updated: 2020/09/26 16:38:27 by rkasubuc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_sort_int_tab(int *tab, int size)
+trimspace_input(char *a)
 {
-	int i;
-	int j;
-	int temp;
-
-	i = 0;
-	while (i < size)
-	{
-		j = size - 1;
-		while (j > i)
-		{
-			if (tab[j] < tab[j - 1])
-			{
-				temp = tab[j];
-				tab[j] = tab[j - 1];
-				tab[j - 1] = temp;
-			}
-			j--;
-		}
-		i++;
-	}
+	char *first[]
+	char *second[]
+	char *third[]
+	
 }
